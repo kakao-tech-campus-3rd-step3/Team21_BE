7f83e3ae94673e5001113db412cfc04896f40ea8
양동현 <dongdong0516@naver.com>
2025-09-02 00:19:26 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/kakao/uniscope/UniScopeApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.uniscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniScopeApplication.class, args);
+    }
+
+}

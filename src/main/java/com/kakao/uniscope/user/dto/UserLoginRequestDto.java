4d87c8202a13a615f9faefc61fba1d82f4f17e3d
최효은 <choehyoeun@choehyoeun-ui-MacBookAir.local>
2025-09-12 12:25:35 +0900

    refactor(dto): builder 추가

--- src/main/java/com/kakao/uniscope/user/dto/UserLoginRequestDto.java
+++ src/main/java/com/kakao/uniscope/user/dto/UserLoginRequestDto.java
@@ -1,11 +1,13 @@
 package com.kakao.uniscope.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Builder
 public class UserLoginRequestDto {
 
     @NotBlank(message = "아이디는 필수입니다")
--- src/main/java/com/kakao/uniscope/user/dto/UserSignupRequestDto.java
+++ src/main/java/com/kakao/uniscope/user/dto/UserSignupRequestDto.java
@@ -3,11 +3,13 @@ package com.kakao.uniscope.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Builder
 public class UserSignupRequestDto {
 
     @NotBlank(message = "이메일은 필수입니다")

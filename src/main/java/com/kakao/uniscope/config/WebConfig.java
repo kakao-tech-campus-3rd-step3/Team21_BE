d72a85397ccad2fcfe4f64aac322b3bc2857ca2b
양동현 <dongdong0516@naver.com>
2025-09-15 15:07:42 +0900

    feat: 프론트와 연동

---
+++ src/main/java/com/kakao/uniscope/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.kakao.uniscope.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://uniscope-git-develop-i3months-projects.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

437367e32d2cb3d66fd66c25b83621fb301b41c1
양동현 <dongdong0516@naver.com>
2025-09-16 21:40:01 +0900

    feat: 교수, 강의 평가 repository 인터페이스 구현

--- src/main/java/com/kakao/uniscope/professor/review/repository/ProfReviewRepository.java
+++ src/main/java/com/kakao/uniscope/professor/review/repository/ProfReviewRepository.java
@@ -2,6 +2,18 @@ package com.kakao.uniscope.professor.review.repository;
 
 import com.kakao.uniscope.professor.review.entity.ProfReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfReviewRepository extends JpaRepository<ProfReview, Long> {
+
+    // 논문 실적 평균
+    @Query("SELECT AVG(pr.thesisPerf) FROM ProfReview pr WHERE pr.professor.profSeq = :profSeq")
+    Double findAvgThesisPerf(@Param("profSeq") Long profSeq);
+
+    // 연구 실적 평균
+    @Query("SELECT AVG(pr.researchPerf) FROM ProfReview pr WHERE pr.professor.profSeq = :profSeq")
+    Double findAvgResearchPerf(@Param("profSeq") Long profSeq);
 }

9d5b2f93dfa62ee177e2cdf03b33c1cf28bbf406
jinwoojwa <jjw6654@gmail.com>
2025-09-18 12:47:37 +0900

    feat: CollegeRepository에 메서드 추가

--- src/main/java/com/kakao/uniscope/college/repository/CollegeRepository.java
+++ src/main/java/com/kakao/uniscope/college/repository/CollegeRepository.java
@@ -1,12 +1,17 @@
 package com.kakao.uniscope.college.repository;
 
 import com.kakao.uniscope.college.entity.College;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CollegeRepository extends JpaRepository<College, Long> {
     List<College> findByUniversity_UnivSeq(Long univSeq);
+
+    @EntityGraph(attributePaths = "departments")
+    Optional<College> findById(Long collegeSeq);
 }

5031280485439e4d2d893645f7bba950ee9ea41b
jinwoojwa <jjw6654@gmail.com>
2025-09-19 03:09:13 +0900

    Refactor: CollegeRepository 인터페이스화
    - CollegeRepository를 인터페이스로 변환
    - JPA Repository를 감싸는 구현체 CollegeRepositoryImpl 구현
    - CollegeService 및 테스트 코드가 인터페이스를 사용하도록 수정

--- src/main/java/com/kakao/uniscope/college/repository/CollegeRepository.java
+++ src/main/java/com/kakao/uniscope/college/repository/CollegeRepository.java
@@ -1,17 +1,13 @@
 package com.kakao.uniscope.college.repository;
 
 import com.kakao.uniscope.college.entity.College;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface CollegeRepository extends JpaRepository<College, Long> {
-    List<College> findByUniversity_UnivSeq(Long univSeq);
+public interface CollegeRepository {
 
-    @EntityGraph(attributePaths = "departments")
     Optional<College> findById(Long collegeSeq);
+
+    List<College> findByUniversitySeq(Long univSeq);
 }
--- src/main/java/com/kakao/uniscope/college/service/CollegeService.java
+++ src/main/java/com/kakao/uniscope/college/service/CollegeService.java
@@ -5,11 +5,9 @@ import com.kakao.uniscope.college.entity.College;
 import com.kakao.uniscope.college.repository.CollegeRepository;
 import com.kakao.uniscope.common.exception.ResourceNotFoundException;
 import com.kakao.uniscope.department.dto.DepartmentDto;
-import com.kakao.uniscope.department.entity.Department;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CollegeService {
@@ -24,11 +22,9 @@ public class CollegeService {
         College college = collegeRepository.findById(collegeSeq)
                 .orElseThrow(() -> new ResourceNotFoundException(collegeSeq + "에 해당하는 단과대학을 찾을 수 없습니다."));
 
-        List<Department> departments = college.getDepartments();
-
-        List<DepartmentDto> departmentDtos = departments.stream()
+        List<DepartmentDto> departmentDtos = college.getDepartments().stream()
                 .map(DepartmentDto::from)
-                .collect(Collectors.toList());
+                .toList();
 
         return new CollegeResponseDto(departmentDtos);
     }

56f420984db817ead78d405019b173a616b101b7
양동현 <dongdong0516@naver.com>
2025-09-25 15:54:15 +0900

    feat: 강의평 개수, 강의평 평균, 강의평 페이징 처리를 위한 repository 쿼리 추가

--- src/main/java/com/kakao/uniscope/lecture/review/repository/LectureReviewRepository.java
+++ src/main/java/com/kakao/uniscope/lecture/review/repository/LectureReviewRepository.java
@@ -2,6 +2,8 @@ package com.kakao.uniscope.lecture.review.repository;
 
 import com.kakao.uniscope.lecture.review.entity.LectureReivew;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Repository;
 public interface LectureReviewRepository extends JpaRepository<LectureReivew, Long> {
 
     List<LectureReivew> findTop3ByLecture_Professor_ProfSeqOrderByCreatedDateDesc(Long profSeq);
+
+    Page<LectureReivew> findByLecture_Professor_ProfSeqOrderByCreatedDateDesc(Long profSeq, Pageable pageable);
 
     // 과제량 평균
     @Query("SELECT AVG(lr.homework) FROM LectureReivew lr JOIN lr.lecture l WHERE l.professor.profSeq = :profSeq")
@@ -23,4 +27,13 @@ public interface LectureReviewRepository extends JpaRepository<LectureReivew, Lo
     // 시험 난이도 평균
     @Query("SELECT AVG(lr.examDifficulty) FROM LectureReivew lr JOIN lr.lecture l WHERE l.professor.profSeq = :profSeq")
     Double findAvgExamDifficulty(@Param("profSeq") Long profSeq);
+
+    // 교수별 전체 강의평 개수
+    @Query("SELECT COUNT(lr) FROM LectureReivew lr JOIN lr.lecture l WHERE l.professor.profSeq = :profSeq")
+    Integer countByProfessorId(@Param("profSeq") Long profSeq);
+
+    // 전체 강의평 평점 평균 (강의평 기준 전체 평점용)
+    @Query("SELECT AVG(CAST((lr.homework + lr.lecDifficulty + lr.examDifficulty) AS DOUBLE) / 3.0) " +
+            "FROM LectureReivew lr JOIN lr.lecture l WHERE l.professor.profSeq = :profSeq")
+    Double findOverallLectureRating(@Param("profSeq") Long profSeq);
 }

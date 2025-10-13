66addad870eb4676020afdbe450f2ebdd6618330
양동현 <dongdong0516@naver.com>
2025-10-13 22:24:29 +0900

    feat: 강의 평가 등록 dto 구현

---
+++ src/main/java/com/kakao/uniscope/lecture/review/dto/LectureReviewResponse.java
@@ -0,0 +1,17 @@
+package com.kakao.uniscope.lecture.review.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+public record LectureReviewResponse(
+        Long reviewSeq,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime timestamp
+) {
+    public static LectureReviewResponse of(Long reviewSeq) {
+        return new LectureReviewResponse(
+                reviewSeq,
+                LocalDateTime.now()
+        );
+    }
+}

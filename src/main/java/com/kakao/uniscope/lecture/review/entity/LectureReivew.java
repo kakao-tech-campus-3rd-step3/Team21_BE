66d295d3a7dfc39c35d4471730d9905c02168b4d
양동현 <dongdong0516@naver.com>
2025-10-13 22:24:45 +0900

    feat: 강의 평가 entity 상세 설명 주석 추가

--- src/main/java/com/kakao/uniscope/lecture/review/entity/LectureReivew.java
+++ src/main/java/com/kakao/uniscope/lecture/review/entity/LectureReivew.java
@@ -12,12 +12,16 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "LECTURE_REVIEW")
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LectureReivew {
 
@@ -26,26 +30,26 @@ public class LectureReivew {
     @Column(name = "LEC_REVIEW_SEQ")
     private Long lecReviewSeq;
 
-    @Column(name = "HOMEWORK")
-    private Integer homework;
+    @Column(name = "SEMESTER")
+    private String semester; // 수강 학기
 
     @Column(name = "LEC_DIFFICULTY")
-    private Integer lecDifficulty;
+    private Integer lecDifficulty; // 강의력
 
     @Column(name = "GRADE_DISTRIBUTION")
-    private Integer gradeDistribution;
-
-    @Column(name = "SEMESTER")
-    private String semester;
+    private Integer gradeDistribution; // 성적 잘주는지
 
     @Column(name = "EXAM_DIFFICULTY")
-    private Integer examDifficulty;
+    private Integer examDifficulty; // 시험 난이도
+
+    @Column(name = "HOMEWORK")
+    private Integer homework; // 과제량
 
     @Column(name = "GROUP_PROJ_REQ")
-    private String groupProjReq;
+    private String groupProjReq; // 팀플 여부
 
     @Column(name = "OVERALL_REVIEW")
-    private String overallReview;
+    private String overallReview; // 강의 총평
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "LEC_SEQ")
